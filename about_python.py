fd572949a03a26585168feb1753769143795de06
akash-here <38589527+akash-here@users.noreply.github.com>
2020-09-21 00:25:16 +0530

    points about python

--- about_python.py
+++ about_python.py
@@ -2,3 +2,5 @@ print("hello all, welcome to this repo")
 print("We will be talking about python")
 
 print("today is second session on python")
+
+print("yes, python is simple and efficent language")
